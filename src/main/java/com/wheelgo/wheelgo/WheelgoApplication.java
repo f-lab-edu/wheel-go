bb8d05efc1394d16d0401ea05c1ff51b9f895de8
ingyeomnote <freeartjin@naver.com>
2024-11-06 23:50:18 +0900

    프로젝트 초기 구축

---
+++ src/main/java/com/wheelgo/wheelgo/WheelgoApplication.java
@@ -0,0 +1,13 @@
+package com.wheelgo.wheelgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheelgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wheelgo/wheelgo/WheelgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wheelgo.wheelgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheelgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
