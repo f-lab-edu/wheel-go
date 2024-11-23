5566b78a47885213ff6655cae912e34ccd268473
ingyeomnote <freeartjin@naver.com>
2024-11-23 12:51:48 +0900

    qrCode 요청 클래스 추가

---
+++ src/main/java/com/wheelgo/request/QRCodeRequest.java
@@ -0,0 +1,13 @@
+package com.wheelgo.request;
+
+public class QRCodeRequest {
+    private String qrCodeData;
+
+    public String getQrCodeData() {
+        return qrCodeData;
+    }
+
+    public void setQrCodeData(String qrCodeData) {
+        this.qrCodeData = qrCodeData;
+    }
+}
