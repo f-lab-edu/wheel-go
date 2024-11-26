0845a8b77e47516c2ad24d955e3769249fd656a3
ingyeomnote <freeartjin@naver.com>
2024-11-26 17:46:55 +0900

    대여 Repository, Service, Controller 생성

---
+++ src/main/java/com/wheelgo/controller/RentalController.java
@@ -0,0 +1,31 @@
+package com.wheelgo.controller;
+
+import com.wheelgo.dto.RentalDTO;
+import com.wheelgo.service.RentalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/rental-service")
+public class RentalController {
+
+    @Autowired
+    private RentalService rentalService;
+
+    // 차량 대여 요청
+    @PostMapping("/rent")
+    public RentalDTO rentVehicle(
+            @RequestParam String qrCode,
+            @RequestParam String vehicleType,
+            @RequestParam Long userId,
+            @RequestParam String username,
+            @RequestParam String location) {
+        return rentalService.rentVehicle(qrCode, vehicleType, userId, username, location);
+    }
+
+    // 차량 반납 요청
+    @PostMapping("/return")
+    public RentalDTO returnVehicle(@RequestParam String qrCode) {
+        return rentalService.returnVehicle(qrCode);
+    }
+}
