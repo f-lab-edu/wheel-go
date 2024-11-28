7e6d48b0da2b10e5487e68eb2e91b8f4046b6d4f
ingyeomnote <freeartjin@naver.com>
2024-11-28 17:34:42 +0900

    기존 rental-service-setup 부분

--- src/main/java/com/wheelgo/controller/MainController.java
+++ src/main/java/com/wheelgo/controller/MainController.java
@@ -2,15 +2,13 @@ package com.wheelgo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class TestController {
-
+public class MainController {
     @GetMapping("/")
-    public String home() {
-        return "index";
+    @ResponseBody
+    public String main(){
+        return "Wheelgo Project";
     }
 }
