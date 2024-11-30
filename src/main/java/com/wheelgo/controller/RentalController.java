ab68a422131dabcb75b1a9350b3238e4f06d8adf
ingyeomnote <freeartjin@naver.com>
2024-11-30 12:53:10 +0900

    로그 추가

--- src/main/java/com/wheelgo/controller/RentalController.java
+++ src/main/java/com/wheelgo/controller/RentalController.java
@@ -2,6 +2,8 @@ package com.wheelgo.controller;
 
 import com.wheelgo.dto.RentalDTO;
 import com.wheelgo.service.RentalService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -9,6 +11,8 @@ import java.util.HashMap;
 @RestController
 @RequestMapping("/api/rental-service")
 public class RentalController {
+
+    private static final Logger logger = LoggerFactory.getLogger(RentalController.class);
 
     private RentalService rentalService;
 
@@ -18,14 +22,22 @@ public class RentalController {
 
     // 차량 대여 요청
     @PostMapping("/rent")
-    public RentalDTO rentVehicle(@RequestBody HashMap rentalDto) {
-        System.out.println("들어오나?" + rentalDto);
-        return null;
+    public RentalDTO rentVehicle(@RequestBody RentalDTO rentalDto) {
+        logger.info("rentVehicle method called with RentalDTO: {}", rentalDto);
+
+        return rentalService.rentVehicle(
+                rentalDto.getVehicleQrCode(),
+                rentalDto.getVehicleType(),
+                rentalDto.getUserId(),
+                rentalDto.getUsername(),
+                rentalDto.getLocation()
+             );
     }
 
     // 차량 반납 요청
     @PostMapping("/return")
     public RentalDTO returnVehicle(@RequestParam String qrCode) {
+        logger.info("returnVehicle method called with qrCode: {}", qrCode);
         return rentalService.returnVehicle(qrCode);
     }
 }
