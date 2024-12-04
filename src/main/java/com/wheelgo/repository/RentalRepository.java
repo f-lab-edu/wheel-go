12b9c3000d99bed5a53f38167c1da686077c0496
ingyeomnote <freeartjin@naver.com>
2024-12-04 17:45:48 +0900

    대여 기능

--- src/main/java/com/wheelgo/repository/RentalRepository.java
+++ src/main/java/com/wheelgo/repository/RentalRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface RentalRepository extends JpaRepository<Rental, Long> {
+    List<Rental> findByVehicleQrCodeAndReturnedFalse(String qrCode);
 }
