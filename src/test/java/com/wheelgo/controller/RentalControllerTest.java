582c242ab80f046cbadb371bac116cbdab04a112
ingyeomnote <freeartjin@naver.com>
2024-12-07 01:49:16 +0900

    카멜표기법으로 변경

--- src/test/java/com/wheelgo/controller/RentalControllerTest.java
+++ src/test/java/com/wheelgo/controller/RentalControllerTest.java
@@ -27,7 +27,7 @@ class RentalControllerTest {
 
     @Test
     @DisplayName("렌트시 유효한 rentalDTO가 부여된 렌트차량 확인후 반환확인")
-    void validate_rental_return_with_dto () throws Exception {
+    void validateRentalReturnWithDto () throws Exception {
         // Given
         RentalDTO rentalDTO = new RentalDTO();
         rentalDTO.setVehicleQrCode("QR123");
@@ -51,7 +51,7 @@ class RentalControllerTest {
 
     @Test
     @DisplayName("반납시 유효한 qrCode가 주어지면 반납된 rentalDTO 반환")
-    void return_rent_dto_with_valid_qr_code() throws Exception {
+    void returnRentDtoWithValidQrCode() throws Exception {
         // Given
         String qrCode = "QR123";
         RentalDTO rentalDTO = new RentalDTO();
