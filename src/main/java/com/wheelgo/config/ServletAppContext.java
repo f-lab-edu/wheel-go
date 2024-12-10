e9ca9187469496c7a19ebdbae57dead71d0983a3
ingyeomnote <freeartjin@naver.com>
2024-12-10 12:08:26 +0900

    @EnableWebMvc를 사용하므로 Swagger 리소스 경로를 명시적으로 추가

--- src/main/java/com/wheelgo/config/ServletAppContext.java
+++ src/main/java/com/wheelgo/config/ServletAppContext.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,5 +18,12 @@ public class ServletAppContext implements WebMvcConfigurer {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters){
         converters.add(new MappingJackson2HttpMessageConverter());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/4.18.2/");
+                // %USERPROFILE%\.gradle\caches\modules-2\files-2.1\org.webjars\swagger-ui\4.18.2
     }
 }
