0f5187dcae9c1153e7dcc0bb23b18b0e3c3666f6
ingyeomnote <freeartjin@naver.com>
2024-12-16 23:04:52 +0900

    내장톰캣 버전을 낮추면서 jakarta에서 javax로 변경

--- src/main/java/com/wheelgo/config/AppConfig.java
+++ src/main/java/com/wheelgo/config/AppConfig.java
@@ -1,6 +1,6 @@
 package com.wheelgo.config;
 
-import jakarta.persistence.EntityManagerFactory;
+import javax.persistence.EntityManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
